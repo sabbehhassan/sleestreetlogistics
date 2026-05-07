db92bfdc8dfebdfd37371ca31c08376e1d770ba2
sabbehhassan <hassansubbeh2446@gmail.com>
2026-05-08 02:34:54 +0500

    Added gitignore

--- backend/server.js
+++ backend/server.js
@@ -3,25 +3,31 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const contactRoutes = require("./routes/contact");
 
+const contactRoutes = require("./routes/contact");
 const agreementRoutes = require("./routes/agreementRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 app.use(bodyParser.json());
 
 app.use(express.static("public"));
 
 app.use("/api", agreementRoutes);
 
+app.use("/api/contact", contactRoutes);
+
 app.get("/", (req, res) => {
   res.send("Agreement Backend Running");
 });
-app.use("/api/contact", contactRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
